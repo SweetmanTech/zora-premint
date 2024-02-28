10b405c143dbff6e41137d087f0fde4aed6bee65
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-28 16:33:23 -0700

    I remove dev comments

--- lib/parseLogEntries.tsx
+++ lib/parseLogEntries.tsx
@@ -2,24 +2,21 @@ const parseLogEntries = (logEntries: any) => {
   const response = {} as any;
 
   logEntries.forEach((log: any) => {
-    // Extract topics[2] - the `to` address, remove leading zeros, and ensure it's in a standard Ethereum address format
     const buyerAddress = '0x' + log.topics[2].substring(26);
-
-    // If the buyerAddress already exists in the response, increment the count, otherwise initialize it
     if (response[buyerAddress]) {
-      response[buyerAddress].editions += 1; // Increment the count for this buyerAddress
+      response[buyerAddress].editions += 1;
     } else {
       response[buyerAddress] = {
         buyer: buyerAddress,
-        editions: 1, // Initialize the count for this new buyerAddress
+        editions: 1,
       };
     }
   });
 
-  // Update totalCreatorReward based on the count of editions for each buyerAddress
+  const hardCodedSoundPrice = 333000000000000n;
   Object.keys(response).forEach((buyerAddress) => {
     const count = response[buyerAddress].editions;
-    response[buyerAddress].totalCreatorReward = (BigInt(count) * 333000000000000n).toString();
+    response[buyerAddress].totalCreatorReward = (BigInt(count) * hardCodedSoundPrice).toString();
   });
 
   return response;
