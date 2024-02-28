de24b8ce0d733b24cd91a5121d5e516f8dfecf87
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-27 20:31:33 -0700

    I remove dev logging.

--- lib/getFarcasterNames.tsx
+++ lib/getFarcasterNames.tsx
@@ -4,43 +4,31 @@ import abi from '@/lib/abi/farcaster-id-registry.json';
 
 const getFarcasterNames = async (rawData: any[]) => {
   const itemsToProcessFurther = [] as any[];
-
-  // Process each item in rawData to add the pfp
   const rawDataWithPfp = await Promise.all(
     rawData.map(async (data: any) => {
       try {
         const owner = data.buyer;
-        console.log('SWEETS farcaster owner', owner);
-        // get FID for Leaderboard
         const farcasterId = await optimismPublicClient.readContract({
           address: FARCASTER_ID_REGISTRY,
           abi,
           functionName: 'idOf',
           args: [owner],
         });
-        console.log('SWEETS farcasterId', farcasterId);
-
         if (farcasterId) {
-          // get User (profilePic) by FID
           const response = await fetch(
             `https://nemes.farcaster.xyz:2281/v1/userDataByFid?fid=${farcasterId}&user_data_type=USER_DATA_TYPE_PFP`,
           );
           const json = await response.json();
           const pfp = json.data.userDataBody.value;
-          console.log('SWEETS data', pfp);
-
-          // Add `pfp` to the current item
           return { ...data, pfp };
         } else {
-          // If no farcasterId, add to itemsToProcessWithENS for ENS lookup
           itemsToProcessFurther.push(data);
-          return null; // Temporarily return null to filter out later
+          return null;
         }
       } catch (err) {
         console.error('SWEETS ERROR', err);
-        // Add to itemsToProcessWithENS if there was an error
         itemsToProcessFurther.push(data);
-        return null; // Temporarily return null to filter out later
+        return null;
       }
     }),
   ).then((results) => results.filter((item) => item !== null));
