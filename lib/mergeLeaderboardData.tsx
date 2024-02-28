0721258d1f059f96ffb02d1a1df01cb0489cfd22
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-28 16:31:11 -0700

    I remove dev comments

--- lib/mergeLeaderboardData.tsx
+++ lib/mergeLeaderboardData.tsx
@@ -1,7 +1,5 @@
 const mergeLeaderboardData = (zoraData: any[], soundData: any[]) => {
   const combinedObj = {} as any;
-
-  // Function to add or update the combined object
   const addToCombined = (entry: any) => {
     const { buyer, totalCreatorReward, editions } = entry;
     if (combinedObj[buyer]) {
@@ -17,14 +15,9 @@ const mergeLeaderboardData = (zoraData: any[], soundData: any[]) => {
       };
     }
   };
-
-  // Process each array
   zoraData.forEach(addToCombined);
   soundData.forEach(addToCombined);
-
-  // Convert the combined object back into an array
   const combinedArray = Object.values(combinedObj);
-
   return combinedArray;
 };
 
