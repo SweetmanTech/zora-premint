3bfed2443201b4895abf707caab17d6116d989cd
sameer rasa <crazzywizard@gmail.com>
2024-03-01 13:39:41 -0700

    using airstack to get info

--- lib/getNames.tsx
+++ lib/getNames.tsx
@@ -1,15 +1,81 @@
-import getEnsNames from './getEnsNames';
-import getFarcasterNames from './getFarcasterNames';
 import getSortedLeaderboard from './getSortedLeaderboard';
-
+const AIRSTACK_API_URL = 'https://api.airstack.xyz/graphql';
 const getNames = async (rawData: any[]) => {
-  const { match: farcasterRows, noMatch } = await getFarcasterNames(rawData);
-  let ensMatch = [];
-  if (noMatch.length > 0) {
-    ensMatch = await getEnsNames(noMatch);
+  const buyers = rawData.map((item) => item.buyer);
+  const query = ` query MyQuery($buyers: [Address!]!, $identities:[Identity!]!){
+    Socials(
+      input: {filter: {identity: {_in: $identities}}, blockchain: ethereum, limit:200}
+    ) {
+      Social {
+        userAssociatedAddresses
+        dappName
+        profileName
+        coverImageURI
+        fnames
+        profileImage
+        profileUrl
+        followerCount
+      }
+    }
+      Domains(
+        input: {filter: {resolvedAddress: {_in: $buyers},isPrimary: {_eq: true}}, blockchain: ethereum, limit:200}
+      ) {
+        Domain {
+          resolvedAddress
+          name
+          isPrimary
+          avatar
+        }
+      }
+    }`;
+  const variables = {
+    buyers,
+    identities: buyers,
+  };
+  try {
+    const res = await fetch(AIRSTACK_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${process.env.NEXT_PUBLIC_AIRSTACK_API_KEY}`, // Add API key to Authorization header
+      },
+      body: JSON.stringify({ query, variables }),
+    });
+    const data = await res.json();
+    const [domains, socials] = [data?.data?.Domains?.Domain, data?.data?.Socials?.Social];
+    const addressToProfile: any = {};
+    const addressToSocial: any = {};
+    domains.forEach((domain: any) => {
+      addressToProfile[domain.resolvedAddress] = {
+        name: domain.name,
+        avatar: domain.avatar,
+      };
+    });
+    socials.forEach((social: any) => {
+      social.userAssociatedAddresses.forEach((address: any) => {
+        addressToSocial[address] = {
+          profileName: social.profileName,
+          profileImage: social.profileImage,
+          profileUrl: social.profileUrl,
+        };
+      });
+    });
+
+    const dataWithNamesAndAvatars = rawData.map((item) => {
+      const social = addressToSocial[item.buyer];
+      const profile = addressToProfile[item.buyer];
+      return {
+        ...item,
+        profileName: social?.profileName || profile?.name,
+        profileImage: social?.profileImage || profile?.avatar,
+        profileUrl: social?.profileUrl,
+      };
+    });
+    return getSortedLeaderboard(dataWithNamesAndAvatars);
+  } catch (err) {
+    console.log(err);
+    return [];
   }
-  const merged = [...farcasterRows, ...ensMatch] as any;
-  return getSortedLeaderboard(merged);
 };
 
 export default getNames;
