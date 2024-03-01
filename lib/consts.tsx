06d245cc0632270de218ba3d2557e3680682ff8b
sameer rasa <crazzywizard@gmail.com>
2024-03-01 16:59:30 -0700

    testing changes

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -1,6 +1,6 @@
 import { FrameMetadataType } from '@coinbase/onchainkit';
 export const FARCASTER_ID_REGISTRY = '0x00000000fc6c5f01fc30151999387bb99a9f489b';
-export const VERCEL_URL = process.env.NEXT_PUBLIC_FRAME_URL || 'http://localhost:3001';
+export const VERCEL_URL = process.env.NEXT_PUBLIC_FRAME_URL || 'http://localhost:3000';
 export const DEFAULT_FRAME = {
   buttons: [
     {
