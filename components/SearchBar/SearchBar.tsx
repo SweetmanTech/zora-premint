45a69dba0b5efaeb8b2ab6a536540a0b5cbcd13f
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-01 17:32:42 -0700

    I remove dev logging.

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,14 +1,11 @@
-import { useRouter } from 'next/navigation';
 import Button from '../Button';
 import { useState } from 'react';
 import { VERCEL_URL } from '@/lib/consts';
 
 const SearchBar = () => {
-  const { push } = useRouter();
   const [creator, setCreator] = useState('');
 
   const handleClick = () => {
-    console.log('SWEETS CLICKED');
     window.open(`${VERCEL_URL}/creator/${creator}`);
   };
 
