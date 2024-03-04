55f85c5893fd070904d8ed073d53873ce6fc8525
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-03 17:17:33 -0700

    I remove unused lib.

--- lib/walletClient.tsx
+++
@@ -1,7 +0,0 @@
-import { WalletClient, createWalletClient, custom } from 'viem';
-import { base } from 'viem/chains';
-
-export const baseWalletClient = createWalletClient({
-  chain: base,
-  transport: custom(window.ethereum!),
-}) as WalletClient;
